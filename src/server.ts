08bd30d42ffc33e2aa4fb7e5522f264dccee4d26
fellipehfa <fellipehf@gmail.com>
2021-03-04 18:53:42 -0300

    Starting

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
-import express from 'express'
-
-const app = express()
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+import { app } from './app'
 
 app.listen(3535, () => console.log("Server's running!⚡⚡⚡"))
